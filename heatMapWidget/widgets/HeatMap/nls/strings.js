cc991064986a688e1f6ab499028b2409399a4a96
AdriSolid <biotekgis@gmail.com>
2018-05-10 19:07:44 +0200

    Updated

--- heatMapWidget/widgets/HeatMap/nls/strings.js
+++ heatMapWidget/widgets/HeatMap/nls/strings.js
@@ -2,7 +2,21 @@ define({
   root: ({
     _widgetLabel: "HeatMap",
     selectLayer: "Select a layer",
-    selectField: "Select a numeric field"
+    selectField: "Select a numeric field",
+    blurRadius: "Blur Radius",
+    maxValue: "Max Value",
+    minValue: "Min Value",
+    drawingTools: "Draw tools",
+    clearGraphics: "Clear",
+    heatMapSlider: "Heat Map Slider",
+    drawingTools: "Get HeatMap using drawing tools",
+    clearGraphicsButton: "Clear",
+    execute: "Execute",
+    removeButton: "Remove all HeatMap layers",
+    alertTitle: "Warning!",
+    alertContent: "Do you want to remove all the heatMap layers?",
+    alertButtonOk: "Yes",
+    alertButtonCancel: "No"
   }),
   "es": 1
 });
